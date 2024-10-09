4f63dff8909ce3feca88d36106ecc11bbd65ed96
yxqf <yxqf500@gmail.com>
2024-10-09 22:02:34 +0800

    newbranch

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main() {
-	std::cout<<"newbash"<<endl;
+	std::cout<<"feature"<<endl;
 	std::cout<<"work2 test"<<endl;
 	std::cout << "Hello, World!" << std::endl;
     return 0;
